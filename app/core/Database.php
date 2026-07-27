d92b8207595d23f5b4ebf9da4c0e7aed2a2e3f12
Dwyane Saludo Sido <disowned.ay@gmail.com>
2026-07-27 16:13:44 +0800

    added .gitignore

--- app/core/Database.php
+++ app/core/Database.php
@@ -1,24 +1,40 @@
-<?php 
-    class Database
-    {
-        private static ?PDO $connection = null;
+<?php
+class Database
+{
+    private static ?PDO $connection = null;
 
-        public static function connect(): PDO
+    public static function connect(): PDO
+    {
+        if (self::$connection === null) 
         {
-            if(self::$connection === null)
+            $host = getenv('MYSQLHOST');
+
+            if ($host !== false) 
+            {
+                $port = getenv('MYSQLPORT') ?: '3306';
+                $dbname = getenv('MYSQLDATABASE');
+                $user = getenv('MYSQLUSER');
+                $pass = getenv('MYSQLPASSWORD');
+            } 
+            else 
             {
                 $config = require '../app/config/database.php';
-
-                self::$connection = new PDO(
-                    "mysql:host={$config['host']};dbname={$config['dbname']}",
-                    $config['user'],
-                    $config['pass']
-                );
-
-                self::$connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $host = $config['host'];
+                $port = $config['port'] ?? '3306';
+                $dbname = $config['dbname'];
+                $user = $config['user'];
+                $pass = $config['pass'];
             }
 
-            return self::$connection;
+            self::$connection = new PDO(
+                "mysql:host={$host};port={$port};dbname={$dbname};charset=utf8mb4",
+                $user,
+                $pass
+            );
+
+            self::$connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
+
+        return self::$connection;
     }
-?>
+}
